4c3ea2bdd33194ffa46231856aeb1652c95717c6
Tristan Bennett <betriistan@gmail.com>
2019-05-08 10:07:04 -0500

    created first two short projects

---
+++ mileage.py
@@ -0,0 +1,7 @@
+print("How many kilometers did you run today?")
+kms = float(input())
+miles = round(kms/1.60934, 2)
+print(f'Okay, you said {kms} kilometers, which is {miles} miles.')
+
+let nums = [4, 9, 3, 22, 19, 109, 39]
+console.log(nums[2:5])
