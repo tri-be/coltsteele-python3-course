b4a20cb0cacb8f0c3ef850c40d4056146f157bd2
Tristan Bennett <betriistan@gmail.com>
2019-05-26 00:30:20 -0500

    completed first two list exercises

---
+++ lists.py
@@ -0,0 +1,16 @@
+# Define list my_stuff, at least 4 elements long with 1 str and 1 float
+my_stuff = ['stuff', 11.11, 'guitar', 'synth']
+# Define list nums, with numbers 1 through 99
+nums = list(range(1,100))
+
+# Accessing List Data
+
+# Don't touch list
+people = ["Hanna", "Louisa", "Clauida", "Angela", "Geoffrey", "aparna"]
+# Change "Hannah" to "Hannah"
+people[0] = "Hannah"
+# Change Geoffrey to Jeffrey
+people[-2] = "Jeffrey"
+# Change "aparna" to "Aparna"
+people[-1] = "Aparna"
+print(people)
